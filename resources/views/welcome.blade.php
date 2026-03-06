057182027f93d5e0e74bf730358b5d71af7bcd90
aluanuel <aluanuel@gmail.com>
2026-03-06 17:36:36 +0300

    modified the index page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,33 @@
 @extends('layouts.default')
 @section('content')
 <!-- Automatic element centering -->
-<div class="login-box">
-  <div class="lockscreen-logo">
-    <h1>Welcome to <br><strong>{{ config('app.name', 'Laravel') }}</strong></h1>
-    <a href="{{ route('login') }}"><img class="rounded float-center" src="{{ asset('theme/dist/img/start.png')}}" alt="User Image"></a>
+<div class="d-flex align-items-center justify-content-center vh-100">
+  <div class="card text-center">
+    <div class="card-body">
+      <h1>Welcome to <br><strong>{{ config('app.name', 'Laravel') }}</strong></h1>
+      <hr class="bg-purple">
+      <!-- <blockquote class="blockquote mb-0"> -->
+        <p class="text-info h5">An intiutive app for Tier 3/4 Microfinance Institutions</p>
+      <!-- </blockquote> -->
+      <div class="row">
+        <div class="col-6">
+          <ul class="text-left text-success ml-2">
+            <li>Disburse loans</li>
+            <li>Track loan recovery</li>
+            <li>Record daily transactions</li>
+            <li>Get instant notifications</li>
+            <li>Generate account statements</li>
+            <li>Calculate business projections</li>
+            <li>Integrate MTN Mobile Money</li>
+          </ul>
+        </div>
+        <div class="col-6">
+          <a href="{{ route('login') }}"><img class="rounded float-center" src="{{ asset('theme/dist/img/start.png')}}" alt="User Image"></a>
+        </div>
+    </div>
+  </div>
+  <div class="card-footer">
+    <small>POWERED BY&nbsp;<a href="https://codestoreinvestments.com" target="blank">CSI Limited</a></small>
   </div>
 </div>
 <!-- /.center -->
